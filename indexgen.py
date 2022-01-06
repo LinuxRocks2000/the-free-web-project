e4cf339d4329c6ca950933bb4e97515c273a06e9
The Buzzard <plupy@awesome.local>
2022-01-06 12:18:43 -0500

    Initial commit, add autogen script and base content

---
+++ indexgen.py
@@ -0,0 +1,19 @@
+## Add index-section to an HTML file.
+import bs4
+
+
+retnr = "<section id = \"index\">\n    <h2>Index</h2>"
+
+with open("rawindex.html") as file:
+    c = file.read()
+    data = bs4.BeautifulSoup(c, "lxml")
+    curLvl = 0
+    for x in data.find_all("section"):
+        pos = 0
+        for i in x["id"]:
+            if i == '.':
+                pos += 1
+        retnr += "    <a style = \"margin-left: " + str(pos * 20) + "px\" href = \"#" + str(pos) + "\">" + x["id"] + ". " + x.find("h2").contents[0] + "</a><br \>\n"
+    retnr += "</section>"
+    open("index.html", "w+").write(c.replace("<!AUTO_INDEX>", retnr))
+print(retnr)
